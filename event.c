5a00a36a0df5b7b1949f95efeea92279c445ac36
Qischer <ntk.lhp@gmail.com>
2024-10-20 18:36:53 -0400

    Build commit: 2024-10-20 18:36:53

--- event.c
+++ event.c
@@ -120,16 +120,18 @@ int eventLoop(SDL_Event* e) {
     SDL_RenderDrawPoint(gRenderer, 
                         player->pos.x, player->pos.y);
 
-    SDL_SetRenderDrawColor(gRenderer, 
-                           0xAF, 0xAF, 0x00, 
-                           SDL_ALPHA_OPAQUE);
-    
     //u32 hitdist = min(rh.dist.x, rh.dist.y);
+    SDL_SetRenderDrawColor(gRenderer, 
+                           0xAF, 0x00, 0x00, 
+                           SDL_ALPHA_OPAQUE);
     SDL_RenderDrawLine(gRenderer, 
                        player->pos.x, player->pos.y, 
                        player->pos.x + rh.dist.x*_cs,
                        player->pos.y + rh.dist.x*_sn);
 
+    SDL_SetRenderDrawColor(gRenderer, 
+                           0x00, 0xAF, 0x00, 
+                           SDL_ALPHA_OPAQUE);
     SDL_RenderDrawLine(gRenderer, 
                        player->pos.x, player->pos.y, 
                        player->pos.x + rh.dist.y*_cs,
