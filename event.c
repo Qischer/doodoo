ca11624567dfbb0cadd0bdf33ba0cbdda751295e
Qischer <ntk.lhp@gmail.com>
2024-10-20 18:39:40 -0400

    Build commit: 2024-10-20 18:39:40

--- event.c
+++ event.c
@@ -77,7 +77,7 @@ int eventLoop(SDL_Event* e) {
     rh.dist.y = sdist.y;
     
     u8 step = 0; // depth step
-    do {
+    while (1) {
         u32 idx = rh.cord.x + rh.cord.y * MAP_COL; 
         if (map[idx] == 1) {rh.hit = 1; break;}
 
@@ -90,7 +90,7 @@ int eventLoop(SDL_Event* e) {
             rh.cord.y += signf(dir_y);
         }
 
-    } while (1);
+    }
 
     //printf("x: %d - y:%d\n", sides.x, sides.y);
     //printf("x: %d - y:%d\n", signf(dir_x), signf(dir_y));
