eeaa63227c96008b3e8e1d2285eade538323e94a
Qischer <ntk.lhp@gmail.com>
2024-10-20 18:48:10 -0400

    Build commit: 2024-10-20 18:48:10

--- event.c
+++ event.c
@@ -81,7 +81,7 @@ int eventLoop(SDL_Event* e) {
         u32 idx = rh.cord.x + rh.cord.y * MAP_COL; 
         if (map[idx] == 1) {rh.hit = 1; break;}
 
-        if (rh.dist.x < rh.dist.y) {
+        if (rh.dist.x <= rh.dist.y) {
             rh.dist.x += delta.x;
             rh.cord.x += signf(dir_x);
         }
